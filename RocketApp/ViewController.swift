92541f41e7c578d03151f2c46f9212a80f9e9256
IgorGalimski <igorg@fm-games.com>
2021-11-20 10:09:10 +0200

    Implement logic

--- RocketApp/ViewController.swift
+++ RocketApp/ViewController.swift
@@ -7,13 +7,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var spaceShipImage: UIImageView!
+    @IBOutlet weak var mainButton: UIButton!
+    
+    var currentSpaceShipStatus: SpaceShipStatus = .Ready
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        SetLanchButtonAppearance()
+    }
+    
+    func SetLanchButtonAppearance()
+    {
+        mainButton.setTitle("Lanch", for: .normal)
+        mainButton.layer.cornerRadius = 25
+    }
+    
+    func LaunchRocket(yPoint: CGFloat)
+    {
+        mainButton.isEnabled = false
+        
+        UIView.animate(withDuration: 1.25,
+                       delay: 0.25,
+                       options: UIView.AnimationOptions.curveEaseIn)
+        {
+            self.spaceShipImage.transform = CGAffineTransform(translationX: .zero, y: yPoint)
+        }
+    completion:
+        {
+            [unowned self] (complete) in
+            if complete
+            {
+                onAnimationFinished()
+            }
+        }
+    }
+    
+    func onAnimationFinished()
+    {
+        switch currentSpaceShipStatus
+        {
+            case .Ready:
+                mainButton.setTitle("Land rocket", for: .normal)
+                currentSpaceShipStatus = .Launched
+            case .Launched:
+                mainButton.setTitle("Launch", for: .normal)
+                currentSpaceShipStatus = .Ready
+        }
+        
+        mainButton.isEnabled = true
     }
 
+    @IBAction func lanchButtonAction(_ sender: Any)
+    {
+        switch currentSpaceShipStatus
+        {
+            case .Ready:
+                LaunchRocket(yPoint: -UIScreen.main.bounds.height)
+            case .Launched:
+                LaunchRocket(yPoint: .zero)
+        }
+    }
+}
 
+enum SpaceShipStatus
+{
+    case Ready
+    case Launched
 }
 
