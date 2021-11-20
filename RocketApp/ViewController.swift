f03543d090de1e6b27fc389f75faff36f153452d
IgorGalimski <igorg@fm-games.com>
2021-11-20 09:05:03 +0200

    Init

---
+++ RocketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketApp
+//
+//  Created by IgorGalimski on 20/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
